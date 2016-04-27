9b98a77936cd0328c6f6662a3d408498280282d0
路闯 <chuang_lu@icloud.com>
2016-04-27 16:25:36 +0800

    这是我第一次玩github

---
+++ gitHub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitHub
+//
+//  Created by ms on 16-4-27.
+//  Copyright (c) 2016年 LuChuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
